0d4b5a44db1d94a77e1c96845b719ac0d5d532fd
JosephGabriel <josephgabriel.jgsm@gmail.com>
2024-01-17 19:56:53 -0300

    imports removed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 import { UsersService } from '../users/users.service';
 import { UserEntity } from '../users/users.entity';
 
 @Module({
-  providers: [AuthService, UsersService],
+  providers: [UsersService],
   imports: [
     TypeOrmModule.forFeature([UserEntity]),
     JwtModule.registerAsync({
